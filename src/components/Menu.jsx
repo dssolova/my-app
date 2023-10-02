023ce5ac00d748b52410b745ec2322df1010e1e1
dssolova <metalgearfun1@gmail.com>
2023-10-02 15:08:53 +0300

    third commi

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link,NavLink } from "react-router-dom";
+import image from "../pictures/1.jpg";
 const Menu=()=>{
 return <div><Link to={"/about"}>Link about </Link>
             <Link to={"/"}>Link home </Link>
             <Link to={"/contact"}>Link contact</Link>
+            <img src={image} alt="Image" />;
         </div>;
 }
 export default Menu ; 
